0c5e85584a921875f6e472aa40bf5b7cde66c1de
laruchsix <thib.ruch@lapost.net>
2020-12-02 19:19:58 +0100

    d

---
+++ POO_Project/src/main/java/Manor/Corpse.java
@@ -0,0 +1,19 @@
+
+package Manor;
+
+public class Corpse {
+    private final String name;
+    private final String description;
+    
+    
+    // constructeurs 
+    public Corpse(String n, String desc){
+        this.name = n;
+        this.description = desc;
+    }
+    
+    
+    public void print(){
+        
+    }
+}
--- POO_Project/src/main/java/Manor/Room.java
+++ POO_Project/src/main/java/Manor/Room.java
@@ -10,6 +10,7 @@ public class Room{
     private ArrayList<Door> doors = new ArrayList<>();
     private ArrayList<Object> objects = new ArrayList<>();
     private ArrayList<Person> persons = new ArrayList<>(); 
+    private ArrayList<Corpse> corpses = new ArrayList<>();
     // l'id de la porte 
     private final int id;
     
@@ -35,6 +36,10 @@ public class Room{
         return ret;
     }
     
+    public ArrayList<Door> getDoors(){
+        return this.doors;
+    }
+    
     public void addDoor(Door d){
         if(!this.isOnDoors(d)){
             this.doors.add(d);
@@ -42,17 +47,102 @@ public class Room{
                 d.addRoom(this);
         }
     }
-    
-    public void addDoor(){
-        Door current = new Door();
-        this.doors.add(current);
-        current.addRoom(this);
-    }
 
     public void removeDoor(Door d){
         this.doors.remove(d);
     }
     
+    public boolean isOnPersons(String n){
+        boolean ret = false;
+        for(Person current : this.persons){
+            if(current.getName() == n ){
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
 
-    //Get door on room
+    public Person getPerson(String n){
+        Person ret = null;
+        for(Person current : this.persons){
+            if(current.getName() == n){
+                ret = current;
+                break;
+            }
+        }
+        return ret;
+    }
+    
+    
+    public void addPerson(Person p){
+        this.persons.add(p);
+    }
+    
+    public void removePerson(Person p){
+        if(this.isOnPersons(p.getName())){
+            p.removeRoom();
+            this.persons.remove(p);
+        }
+        else{
+            System.out.println(p.getName() + " isn't on " + this.name + "!");
+        }
+    }
+    
+    public void addDoors(ArrayList<Door> ds){
+        for(Door current : ds){
+            if(!(this.isOnDoors(current))){
+                this.doors.add(current);
+            }
+        }
+    }
+    
+    public boolean isOnObjects(Object o){
+        boolean ret = false;
+        for(Object current : this.objects){
+            if(current == o){
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
+    
+    public void addObject(Object o){
+        if(this.isOnObjects(o))
+            System.err.print("This object is already present in the room !");
+        else{
+            this.objects.add(o);
+        }
+    }
+    
+    public void removeObject(Object o){
+        if(this.isOnObjects(o))
+            this.objects.remove(o);
+        else
+            System.out.println(o.getName() + " isn't on " + this.name + "!");
+    }
+    
+    public boolean isOnCorpse(Corpse c){
+        boolean ret = false;
+        for(Corpse current : this.corpses){
+            if(current == c){
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
+    
+    public void addCorpse(Corpse c){
+        if(this.isOnCorpse(c)){
+            System.err.print("this corpse is already present in the room !");
+        }
+        else
+            this.corpses.add(c);
+    }
+    
+    public void print(){
+        
+    }
 }
