6838a45219917243402d75e2231434d83f82cccc
N0ITUL0V3R <daniel.rodriguezhernando@gmail.com>
2020-12-03 17:34:12 +0100

    d
    d

--- POO_Project/src/test/java/Manor/ConsumableIT.java
+++ POO_Project/src/test/java/Manor/ConsumableIT.java
@@ -20,9 +20,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ConsumableIT {
     
     public ConsumableIT() {
-        private NPC testSubject1;
-        private NPC testSubject2;
-        private NPC testSubject3;
+        private Npc testSubject1;
+        private Npc testSubject2;
+        private Npc testSubject3;
+        private Npc testSubject4;
         private Consumable healingPotion = new Consumable("GRANDMA'S COOKIES", "THOSE COOKIES WERE MADE WITH LOVE BY ONE OF THE MANOR RESIDENTS GRANDMA", 20);
         private Consumable hurtingPotion = new Consumable("MOLOTOV COCKTAIL", "THIS IS NOT VERY SAFE TO DRINK, I RECOMMEND YOU TO USE IT AGAINST YOUR ENEMIES", -35);
     }
@@ -40,6 +41,7 @@ public class ConsumableIT {
         testSubject1 = new NPC("FREDY", 100, null);
         testSubject2 = new NPC("REMY", 20, null);
         testSubject3 = new NPC("CORPSE", 0, null);
+        testSubject3 = null;
     }
     
     @AfterEach
@@ -53,6 +55,8 @@ public class ConsumableIT {
     @Test
     public void testUse1() {
         
+        hurtingPotion.setOwner(testSubject2);
+        assertNotNull(hurtingPotion.canUse());
         hurtingPotion.use(testSubject1);
         assertFalse(hurtingPotion.canUse()); //test on peut pas le reutiliser
         assertEquals(testSubject.getCurrentHp(), 65);
@@ -62,6 +66,8 @@ public class ConsumableIT {
     @Test
     public void testUse2() {
         
+        hurtingPotion.setOwner(testSubject2);
+        assertNotNull(hurtingPotion.canUse());
         healingPotion.use(testSubject1);
         assertFalse(healingPotion.canUse());
         assertEquals(testSubject.getCurrentHp(), 100);
@@ -71,6 +77,8 @@ public class ConsumableIT {
     @Test 
     public void testUse3() {
         
+        hurtingPotion.setOwner(testSubject1);
+        assertNotNull(hurtingPotion.canUse());
         hurtingPotion.use(testSubject2);
         assertFalse(hurtingPotion.canUse());
         assertEquals(testSubject.getCurrentHp(), 0);
@@ -78,6 +86,8 @@ public class ConsumableIT {
      
     @Test 
     public void testUse4() {
+        hurtingPotion.setOwner(testSubject1);
+        assertNotNull(hurtingPotion.canUse());
         hurtingPotion.use(testSubject3);
         assertTrue(hurtingPotion.canUse());
         assertEquals(testSubject.getCurrentHp(), 0);   
@@ -85,8 +95,25 @@ public class ConsumableIT {
 
     @Test 
     public void testUse5() {
+        hurtingPotion.setOwner(testSubject1);
+        assertNotNull(hurtingPotion.canUse());
         healingPotion.use(testSubject3);
         assertTrue(healingPotion.canUse());
         assertEquals(testSubject.getCurrentHp(), 0);    
+    }
+
+    @Test 
+    public void testUse5() {
+        hurtingPotion.setOwner(testSubject1);
+        assertNotNull(hurtingPotion.canUse());
+        healingPotion.use(testSubject3);
+        assertTrue(healingPotion.canUse());
+        assertEquals(testSubject.getCurrentHp(), 0);    
+    }
+        
+    @Test 
+    public void testUse6() {
+        hurtingPotion.setOwner(testSubject4);
+        assertNull(hurtingPotion.canUse());
     }
 }
